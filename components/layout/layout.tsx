5b276b8eed40a0f5fd974860c1cb0b9a3a73667e
John Evans <john@wearelighthouse.com>
2022-11-18 19:48:51 +0000

    Fix minor rawData prop not existing issue

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -4,7 +4,7 @@ import layoutData from "../../content/global/index.json";
 
 /* Disable no-unused-vars because tina's files send things to the currently unused data property */
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-export const Layout = ({ data = layoutData, children }) => {
+export const Layout = ({ data = layoutData, rawData = null, children }) => {
   return (
     <>
       <Head>
