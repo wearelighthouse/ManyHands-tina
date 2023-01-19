546124fe71b96200331ce42b943672639041932a
John Evans <john@wearelighthouse.com>
2023-01-19 20:05:45 +0000

    Attempt to use correct ManyHands favicon

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -2,14 +2,26 @@ import React from "react";
 import Head from "next/head";
 import layoutData from "../../content/global/index.json";
 
+const prefix = process.env.PREFIX ?? '';
+
 /* Disable no-unused-vars because tina's files send things to the currently unused data property */
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 export const Layout = ({ data = layoutData, rawData = null, children }) => {
   return (
     <>
       <Head>
-        <title>ManyHands</title>
+        <title>ManyHands: The problem solving event for product people 🤘</title>
+        <meta name="description" content="A collaborative problem solving event for product pros. Work together on real-world UX problems online."></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+        <link rel="apple-touch-icon" sizes="180x180" href={`${prefix}/assets/img/apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${prefix}/assets/img/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${prefix}/assets/img/favicon-16x16.png`} />
+
+        <meta property="og:image" content={`${prefix}/assets/img/manyhands-preview.png`} />
+        <meta property="og:image:width" content="1024" />
+        <meta property="og:image:height" content="538" />
+        <meta property="og:image:type" content="image/png" />
       </Head>
       {children}
     </>
