0fb9859af469c7f041fc5197ba8570329b420a03
John Evans <john@wearelighthouse.com>
2023-01-19 16:05:12 +0000

    Slightly better 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,22 @@
 import { Hero } from "../components/blocks/hero";
 
+const prefix = process.env.PREFIX ?? '';
+
 export default function FourOhFour() {
   return (
-    <Hero
-      data={{
-        color: "default",
-        headline: "404 – Page Not Found",
-        text: "Oops! It seems there's nothing here, how embarrassing.",
-        actions: [
-          {
-            label: "Return Home",
-            type: "button",
-            icon: true,
-            link: "/",
-          },
-        ],
-      }}
-    />
+    <main className="min-h-screen min-h-[100dvh] grid">
+      <div className="grid bg-mimosa px-6 py-14 max-tablet:pb-6 tablet:px-16 tablet:m-4 text-center">
+        <div className="flex justify-center items-start">
+          <a href={`${prefix}/`} aria-label="Home">
+            <img className="mx-auto" src={`${prefix}/assets/manyhands-logo.svg`} alt="Many Hands" width="188px" height="34px"/>
+          </a>
+        </div>
+
+        <div className="max-tablet:grid content-start justify-center whitespace-nowrap ">
+          <h1 className="h1 mx-auto">404 - Page not found</h1>
+          <a href={`${prefix}/`} className="button h-12 my-8">Return to the ManyHands Homepage</a>
+        </div>
+      </div>
+    </main>
   );
 }
