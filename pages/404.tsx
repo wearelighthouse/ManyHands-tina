72c192275df97bd544d1032873b7c242ba52bf35
John Evans <john@wearelighthouse.com>
2023-01-19 16:14:42 +0000

    Swap 404.js for 404.tsx, hoping it will fix 'process is not defined' error

---
+++ pages/404.tsx
@@ -0,0 +1,20 @@
+const prefix = process.env.PREFIX ?? '';
+
+export default function FourOhFour() {
+  return (
+    <main className="min-h-screen min-h-[100dvh] grid">
+      <div className="grid bg-mimosa px-6 py-14 max-tablet:pb-6 tablet:px-16 tablet:m-4 text-center">
+        <div className="flex justify-center items-start">
+          <a href={`${prefix}/`} aria-label="Home">
+            <img className="mx-auto" src={`${prefix}/assets/manyhands-logo.svg`} alt="Many Hands" width="188px" height="34px"/>
+          </a>
+        </div>
+
+        <div className="max-tablet:grid content-start justify-center whitespace-nowrap ">
+          <h1 className="h1 mx-auto">404 - Page not found</h1>
+          <a href={`${prefix}/`} className="button h-12 my-8">Return to the ManyHands Homepage</a>
+        </div>
+      </div>
+    </main>
+  );
+}
