0d475790fc84b88df883b19859ef1f14306459c0
John Evans <john@wearelighthouse.com>
2023-04-19 11:46:44 +0100

    Fix minutes not showing on times formatted with formatTime util

--- components/util/date-time.ts
+++ components/util/date-time.ts
@@ -23,10 +23,12 @@ const createEndTime = (dateTime: Date): Date => {
 
 export const formatTime = (dateTime: Date, dateTimeEnd: Date = createEndTime(dateTime)): string => {
   const start = dateTime
-    .toLocaleTimeString('en-GB', { hour: 'numeric', hour12: true })
-    .replace(/[^\d]+/, '');  // Remove non-alphanumeric 'am' or 'pm' from start time
+    .toLocaleTimeString('en-GB', { hour: 'numeric', minute: 'numeric', hour12: true })
+    .replace(':00', '') // Remove unecessary minutes if e.g. 7:00pm
+    .replace(/[a-z]+/, '');  // Remove 'am' or 'pm' from start time
   const end = dateTimeEnd
-    .toLocaleTimeString('en-GB', { hour: 'numeric', hour12: true })
+    .toLocaleTimeString('en-GB', { hour: 'numeric', minute: 'numeric', hour12: true })
+    .replace(':00', '') // Remove unecessary minutes if e.g. 7:00pm
     .replace(' ', ' ');  // Swap standard space with narrow no-break space (U+202f) to match Chromes toLocaleTimeString
   return `${start} - ${end} (UK time)`;
 }
