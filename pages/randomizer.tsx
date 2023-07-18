8e3d9f2bde3b33f29833161a4ad8f885bc49d40c
mrhaste <tom@wearelighthouse.com>
2023-07-18 16:09:35 +0100

    Revert "Change randomizer buttons"

--- pages/randomizer.tsx
+++ pages/randomizer.tsx
@@ -163,8 +163,9 @@ export default function Randomizer() {
           </div>
 
           <div className="flex flex-wrap items-end justify-end content-end gap-4 static z-10">
-            <button className="button h-10 reset">◼</button>
-            <button className="button h-10 run">▶</button>
+            <button className="button h-10 reset">Reset</button>
+            <button className="button h-10 run">Run</button>
+            <button className="button h-10 overflow">Toggle Overflow</button>
           </div>
         </div>
       </main>
