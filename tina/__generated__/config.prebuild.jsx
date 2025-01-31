52b0b3fe231d75b4af59198c181bd426d91119fa
John Evans <john@wearelighthouse.com>
2025-01-31 03:29:13 +0000

    Hopefully resync with TinaCMS by running dev command locally

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -1077,7 +1077,7 @@ var queries = (client2) => {
 };
 
 // tina/__generated__/client.ts
-var client = createClient2({ url: "http://localhost:4001/graphql", token: "a7cfd0e5925d0bf18f9e48201fc174e53325c39a", queries });
+var client = createClient2({ cacheDir: "/home/burntcustard/Work/manyhands-tina/tina/__generated__/.cache/1738293747658", url: "https://content.tinajs.io/1.5/content/0064d9a4-e65d-4a88-8310-7f469f0e00ff/github/main", token: "a7cfd0e5925d0bf18f9e48201fc174e53325c39a", queries });
 
 // components/blocks/event-list.tsx
 import Link from "next/link";
